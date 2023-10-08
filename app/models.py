4ecac7c4a99df727300e40f7c9a5d853394843bc
islam <cloplop091@gmail.com>
2023-10-08 21:26:56 +0300

    models update

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from mptt.models import MPTTModel
 
 class MenuModel(MPTTModel):
     """Древовидное меню"""
-    name = models.CharField('Название', max_length=150, help_text='Максимум 100 символов')
+    name = models.CharField('Название', max_length=150, unique=True, help_text='Максимум 100 символов')
     named_url = models.SlugField('URL', max_length=150, unique=True)
     parent = TreeForeignKey('self', verbose_name='Выберете поле', help_text='Выберете пункт', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
