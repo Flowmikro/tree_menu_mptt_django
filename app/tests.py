73716e54fecf80ba0dec1e62242e285c3f2ff6c8
islam <cloplop091@gmail.com>
2023-10-09 02:14:47 +0300

    Test

--- app/tests.py
+++ app/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import MenuModel
 
-class MenuTest(TestCase):
 
+class MenuTestCase(TestCase):
     def setUp(self):
         self.menu = MenuModel.objects.create(name='Menu', named_url='menu', parent=None)
 
@@ -17,11 +17,9 @@ class MenuTest(TestCase):
 
     def test_menu_hierarchy(self):
         child_menu = MenuModel.objects.create(name='Child Menu', named_url='child-menu', parent=self.menu)
-
         url = reverse('menu', args=['menu'])
         response = self.client.get(url)
 
         self.assertContains(response, self.menu.name)
         self.assertContains(response, child_menu.name)
-
         self.assertContains(response, '<ul class="children">')
